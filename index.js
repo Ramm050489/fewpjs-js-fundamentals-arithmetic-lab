be749cec6202aad7345810c52a8eccbe300ce7b1
Ramon <ramm050489@gmail.com>
2021-05-05 15:28:13 -0400

    commit tree a2ce3984987091d3fc7a64122990ca72cecd9404

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+
+function multiplyBy() {
+    num1 = 31
+    num2 = 2
+    return num1*num2
+}
+let num1 = 31
+let num2 = 2
+let multiply = multiplyBy(num1*num2)
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 2
+let num4 = 2
+
+let mod = 16 / 4
+
+let max = Math.max(5,10,15,20);
+
